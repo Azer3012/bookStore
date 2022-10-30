be3c268694c9d11e5ee9c880eb0af21295c66d7e
azer <azer@azers-MacBook-Pro.local>
2022-10-30 22:58:41 +0400

    login page design

--- clientBookStore/app/components/CustomInput.js
+++ clientBookStore/app/components/CustomInput.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import helpers from '../helpers/helpers';
 
-const CustomInput = () => {
+import EyeIcon from 'react-native-vector-icons/Feather';
+
+const CustomInput = ({placeholder, icon, password,onPress}) => {
+  const [hide, setHide] = useState(false);
   return (
-    <View>
-      <Text>CustomInput</Text>
+    <View style={styles.container}>
+      <TextInput
+      
+      style={styles.input}
+       placeholder={placeholder}
+       secureTextEntry={password}
+       />
+      {password && (
+        <TouchableOpacity onPress={()=>setHide(!hide)}>
+          <EyeIcon size={20} name={hide ? 'eye' : 'eye-off'} />
+        </TouchableOpacity>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default CustomInput
+export default CustomInput;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    height: helpers.px(40),
+    borderRadius: helpers.px(20),
+    flexDirection: 'row',
+    paddingHorizontal: helpers.px(16),
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input:{
+    ...helpers.fontStyle("Regular",14)
+  }
+});
--- clientBookStore/app/pages/Login/index.js
+++ clientBookStore/app/pages/Login/index.js
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import helpers from '../../helpers/helpers';
+import colors from '../../values/colors';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Image
+          style={styles.image}
+          source={require('../../assets/images/book.png')}
+        />
+        <View style={styles.inputContainer}>
+          <CustomInput placeholder={'User name'} />
+        </View>
+        <View style={styles.inputContainer}>
+          <CustomInput placeholder={'Password'} password={true} />
+        </View>
+        <View style={styles.btnContainer}>
+          <CustomButton
+            text={'Login'}
+            color={colors.white}
+            indicatorColor={colors.white}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: helpers.px(136),
+    height: helpers.px(100),
+    tintColor: colors.main,
+    marginBottom: helpers.px(20),
+  },
+  inputContainer: {
+    width: helpers.px(190),
+    marginBottom: helpers.px(16),
+  },
+  btnContainer: {
+    width: helpers.px(195),
+    marginBottom: helpers.px(16),
+  },
+});
